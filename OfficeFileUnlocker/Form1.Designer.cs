afe022ceddbda1c366663ba64fe0f49210bb3aa2
Adam Akkari <gr9343@wayne.edu>
2019-03-26 11:06:24 -0400

    Added base solution files + algorith attempt

---
+++ OfficeFileUnlocker/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OfficeFileUnlocker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_browse = new System.Windows.Forms.Button();
+            this.lbl_filename = new System.Windows.Forms.Label();
+            this.bt_unlock = new System.Windows.Forms.Button();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_browse
+            // 
+            this.bt_browse.Location = new System.Drawing.Point(12, 12);
+            this.bt_browse.Name = "bt_browse";
+            this.bt_browse.Size = new System.Drawing.Size(83, 23);
+            this.bt_browse.TabIndex = 0;
+            this.bt_browse.Text = "Browse file";
+            this.bt_browse.UseVisualStyleBackColor = true;
+            this.bt_browse.Click += new System.EventHandler(this.bt_browse_Click);
+            // 
+            // lbl_filename
+            // 
+            this.lbl_filename.AutoSize = true;
+            this.lbl_filename.Location = new System.Drawing.Point(12, 38);
+            this.lbl_filename.Name = "lbl_filename";
+            this.lbl_filename.Size = new System.Drawing.Size(55, 13);
+            this.lbl_filename.TabIndex = 1;
+            this.lbl_filename.Text = "File name:";
+            // 
+            // bt_unlock
+            // 
+            this.bt_unlock.Location = new System.Drawing.Point(12, 54);
+            this.bt_unlock.Name = "bt_unlock";
+            this.bt_unlock.Size = new System.Drawing.Size(83, 23);
+            this.bt_unlock.TabIndex = 2;
+            this.bt_unlock.Text = "Unlock!";
+            this.bt_unlock.UseVisualStyleBackColor = true;
+            this.bt_unlock.Click += new System.EventHandler(this.bt_unlock_Click);
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(12, 80);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(86, 13);
+            this.lbl_password.TabIndex = 3;
+            this.lbl_password.Text = "Password found:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 125);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.bt_unlock);
+            this.Controls.Add(this.lbl_filename);
+            this.Controls.Add(this.bt_browse);
+            this.Name = "MainForm";
+            this.Text = "Office file unlocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_browse;
+        private System.Windows.Forms.Label lbl_filename;
+        private System.Windows.Forms.Button bt_unlock;
+        private System.Windows.Forms.Label lbl_password;
+    }
+}
+
---
+++ OfficeFileUnlocker/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace OfficeFileUnlocker
+{
+    public partial class MainForm : Form
+    {
+        string filepath = @"C:\";
+        string filename = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_browse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = filepath;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filename = openFileDialog.FileName;
+                    lbl_filename.Text = "File name: " + filename;
+                }
+            }
+        }
+
+        private void bt_unlock_Click(object sender, EventArgs e)
+        {
+            Application excel = new Application();
+            Workbook wb = excel.Workbooks.Open(filename);
+            Worksheet sheet = (Worksheet)wb.ActiveSheet;
+            char[] pwd = new char[13];
+            
+            //algo du futur
+            for (char i = 'A'; i < 67; ++i)
+                for (char j = 'A'; j < 67; ++j)
+                    for (char k = 'A'; k < 67; ++k)
+                        for (char l = 'A'; l < 67; ++l)
+                            for (char m = 'A'; m < 67; ++m)
+                                for (char n = 'A'; n < 67; ++n)
+                                    for (char o = 'A'; o < 67; ++o)
+                                        for (char p = 'A'; p < 67; ++p)
+                                            for (char q = 'A'; q < 67; ++q)
+                                                for (char r = 'A'; r < 67; ++r)
+                                                    for (char s = 'A'; s < 67; ++s)
+                                                        for (char t = ' '; t < 127; ++t)
+                                                    {
+                                                        try
+                                                            {
+                                                                pwd[0] = i;
+                                                                pwd[1] = j;
+                                                                pwd[2] = k;
+                                                                pwd[3] = l;
+                                                                pwd[4] = m;
+                                                                pwd[5] = n;
+                                                                pwd[6] = o;
+                                                                pwd[7] = p;
+                                                                pwd[8] = q;
+                                                                pwd[9] = r;
+                                                                pwd[10] = s;
+                                                                pwd[11] = t;
+                                                                pwd[12] = Convert.ToChar(0);
+                                                                sheet.Unprotect(pwd);
+                                                                if (sheet.ProtectContents == false)
+                                                                {
+                                                                    lbl_password.Text = "Password: " + pwd;
+                                                                    return;
+                                                                }
+                                                            }
+                                                            catch
+                                                            {
+
+                                                            }
+                                                    }
+        }
+    }
+}
